ea1fa8b1fed421cb7d238854a4e91b47d2ba91c2
张小玲 <409273291@qq.com>
2016-11-13 18:55:34 +0800

    增加websocket相关东西

---
+++ src/main/java/org/ceeker/web/sbootm/common/task/WebSocketTask.java
@@ -0,0 +1,21 @@
+package org.ceeker.web.sbootm.common.task;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebSocketTask {
+	@Autowired
+	private SimpMessagingTemplate template;
+
+	@Scheduled(fixedRate = 2000)
+	public void echo2send() {
+		String time = new Date().toLocaleString();
+		System.out.println(time);
+		template.convertAndSend("/topic/echo_service", "测试内容，" + time);
+	}
+}
